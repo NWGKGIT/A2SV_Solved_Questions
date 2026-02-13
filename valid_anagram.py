24aab31d793dc67d76cbb0394d4fdfb4dcd292ab
Nahom Gebremichael <119551720+NWGKGIT@users.noreply.github.com>
2026-02-14 00:01:39 +0300

    6th batch

---
+++ valid_anagram.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counts=Counter(s)
+        countt=Counter(t)
+        return counts==countt
